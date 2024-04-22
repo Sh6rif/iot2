f05692e9772d390a22c96d30b09c0a5082c7f639
Sherif Alaa <sherefalex34@gmail.com>
2024-04-23 00:45:20 +0200

    new port

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ app.post("/change-led-status", async (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(9000, () => {
+  console.log("Server is running on port 9000");
 });
